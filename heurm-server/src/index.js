88ff31c60a75d7900924aa929cb76a2860ee49ca
tintoll <blue@gmail.com>
2017-12-05 14:08:11 +0900

    미들웨어 개념 주석

--- heurm-server/src/index.js
+++ heurm-server/src/index.js
@@ -3,7 +3,8 @@ const app = new Koa();
 
 
 // app.use() 는 미들웨어를 등록하여 줍니다. 
-// 미들웨어란? 
+// 미들웨어란? 요청이 와서 응답을 주기 전까지 해야할 일들을 담음 함수
+// 아래는 어떤한 요청이 들어와도 무조건 Helo Koa를 body에 넣어준다. 
 app.use(ctx => {
     ctx.body = 'Hello Koa';
 });
