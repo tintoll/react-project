9d5b48108d38f5ce464f1061d781a32cb85448a2
tintoll <belselios@gmail.com>
2019-02-01 07:59:07 +0900

    5.4

--- heurm-client/src/pages/Auth.js
+++ heurm-client/src/pages/Auth.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as baseActions from 'redux/modules/base';
+import { dispatch } from 'rxjs/internal/observable/pairs';
+
 
 class Auth extends Component {
+  // 페이지 진입할 때 헤더를 비활성화 
+  componentWillMount() {
+    this.props.BaseActions.setHeaderVisibility(false);
+  }
+  
+  // 페이지에서 벗어 날때 다시 활성화 
+  componentWillUnmount() {
+    this.props.BaseActions.setHeaderVisibility(true);
+  }
+  
   render() {
     return (
       <div>
@@ -10,4 +25,11 @@ class Auth extends Component {
   }
 }
 
-export default Auth;
+export default connect(
+  (state) => ({
+
+  }),
+  (dispatch) => ({
+    BaseActions : bindActionCreators(baseActions, dispatch)
+  })
+)(Auth);
