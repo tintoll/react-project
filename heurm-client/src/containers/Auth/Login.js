b3fc1c8a879a766af3aae032d5faff80d8e2614f
tintoll <belselios@gmail.com>
2019-02-08 08:34:15 +0900

    5-7

--- heurm-client/src/containers/Auth/Login.js
+++ heurm-client/src/containers/Auth/Login.js
@@ -6,15 +6,53 @@ import {
   RightAlignedLink
 } from "components/Auth";
 
+// 리덕스 연동하기 위해 필요한 부분 
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as authActions from "redux/modules/auth";
+import { dispatch } from 'rxjs/internal/observable/range';
+
 class Login extends Component {
+
+  handleChange = (e) => {
+    const { AuthActions } = this.props;
+    const { name , value} = e.target;
+    AuthActions.changeInput({
+      name, 
+      value,
+      form : 'login'
+    });
+  }
+
+  componentWillUnmount() {
+    const { AuthActions } = this.props;
+    AuthActions.initializeForm("login");
+  }
+
   render() {
+    // form 에서 email 과 password 값을 읽어옴
+    const {email, password} = this.props.form.toJS();
+    const { handleChange } = this;
+
     return <AuthContent title="로그인">
-        <InputWithLabel label="이메일" name="email" placeholder="이메일" />
-        <InputWithLabel label="비밀번호" name="password" placeholder="비밀번호" type="password"/>
+        <InputWithLabel label="이메일" name="email" placeholder="이메일" 
+          value={email}
+          onChange={handleChange} />
+        <InputWithLabel label="비밀번호" name="password" placeholder="비밀번호" type="password"
+          value={password}
+          onChange={handleChange}/>
         <AuthButton>로그인</AuthButton>
         <RightAlignedLink to="/auth/register">회원가입</RightAlignedLink>
       </AuthContent>;
   }
 }
 
-export default Login;
+// export default Login;
+export default connect(
+  (state) => ({
+    form : state.auth.getIn(['login', 'form'])
+  }),
+  (dispatch) => ({
+    AuthActions : bindActionCreators(authActions, dispatch)
+  })
+)(Login);
--- heurm-client/src/containers/Auth/Register.js
+++ heurm-client/src/containers/Auth/Register.js
@@ -5,14 +5,48 @@ import {
   AuthButton,
   RightAlignedLink
 } from "components/Auth";
+// 리덕스 연동하기 위해 필요한 부분 
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as authActions from "redux/modules/auth";
+import { dispatch } from 'rxjs/internal/observable/range';
+
+
 class Register extends Component {
+  handleChange = (e) => {
+    const { AuthActions } = this.props;
+    const { name, value } = e.target;
+
+    AuthActions.changeInput({
+      name,
+      value,
+      form: 'register'
+    });
+  }
+
+  componentWillUnmount() {
+    const {AuthActions} = this.props;
+    AuthActions.initializeForm("register");
+  }
+
   render() {
+    const { email, username, password, passwordConfirm } = this.props.form.toJS();
+    const { handleChange } = this;
+
     return (
       <AuthContent title="회원가입">
-        <InputWithLabel label="이메일" name="email" placeholder="이메일" />
-        <InputWithLabel label="아이디" name="username" placeholder="아이디" />
-        <InputWithLabel label="비밀번호" name="password" placeholder="비밀번호" type="password" />
-        <InputWithLabel label="비밀번호 확인" name="passwordConfirm" placeholder="비밀번호 확인" type="password" />
+        <InputWithLabel label="이메일" name="email" placeholder="이메일" 
+          value={email}
+          onChange={handleChange}/>
+        <InputWithLabel label="아이디" name="username" placeholder="아이디" 
+          value={username}
+          onChange={handleChange}/>
+        <InputWithLabel label="비밀번호" name="password" placeholder="비밀번호" type="password" 
+          value={password}
+          onChange={handleChange}/>
+        <InputWithLabel label="비밀번호 확인" name="passwordConfirm" placeholder="비밀번호 확인" type="password" 
+          value={passwordConfirm}
+          onChange={handleChange}/>
         <AuthButton>회원가입</AuthButton>
         <RightAlignedLink to="/auth/login">로그인</RightAlignedLink>
       </AuthContent>
@@ -20,4 +54,11 @@ class Register extends Component {
   }
 }
 
-export default Register;
+export default connect(
+  (state) => ({
+    form: state.auth.getIn(['register', 'form'])
+  }),
+  (dispatch) => ({
+    AuthActions: bindActionCreators(authActions, dispatch)
+  })
+)(Register);
