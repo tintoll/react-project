a6c2acda850b3745ca9f164bf4876e10340dbc33
tintoll <belselios@gmail.com>
2019-02-12 08:27:48 +0900

    6-4

--- heurm-client/src/containers/Auth/Register.js
+++ heurm-client/src/containers/Auth/Register.js
@@ -10,9 +10,11 @@ import {
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as authActions from "redux/modules/auth";
+import * as userActions from "redux/modules/user";
 import { dispatch } from 'rxjs/internal/observable/range';
 import { isEmail, isLength, isAlphanumeric } from "validator";
 import debounce from "lodash/debounce";
+import storage from "lib/storage";
 
 
 class Register extends Component {
@@ -104,6 +106,41 @@ class Register extends Component {
     check(value);
   }
 
+  handleLocalRegister = async () => {
+    const { form, AuthActions, UserActions, error, history } = this.props;
+    const { email, username, password, passwordConfirm} = form.toJS();
+
+    const { validate } = this;
+    if(error) return; // 현재 에러가 있는 상태라면 진행하지 않음.
+    if (!validate['email'](email) || !validate['username'](username) ||
+      !validate['password'](password) || !validate['passwordConfirm'](passwordConfirm) ){
+        return;
+    }
+
+    try {
+      await AuthActions.localRegister({
+        email, username, password
+      });
+      const loggedInfo = this.props.result.toJS();
+      
+      storage.set('loggedInfo', loggedInfo);
+      UserActions.setLoggedInfo(loggedInfo);
+      UserActions.setValidated(true);
+
+      history.push('/'); //회원가입 성공시 홈페이지 이동 
+
+    } catch(e) {
+      if(e.response.status === 409) {
+        const { key } = e.response.data;
+        const message = key === 'email' ? '이미 존재하는 이메일입니다.' : '이미 존재하는 아이디입니다.';
+        return this.setError(message);
+      }
+
+      this.setError('알 수 없는 에러가 발생했습니다.');
+    }
+
+  }
+
   componentWillUnmount() {
     const {AuthActions} = this.props;
     AuthActions.initializeForm("register");
@@ -112,7 +149,7 @@ class Register extends Component {
   render() {
     const { error } = this.props;
     const { email, username, password, passwordConfirm } = this.props.form.toJS();
-    const { handleChange } = this;
+    const { handleChange, handleLocalRegister } = this;
 
     return (
       <AuthContent title="회원가입">
@@ -131,7 +168,7 @@ class Register extends Component {
         {
           error && <AuthError>{error}</AuthError>
         }  
-        <AuthButton>회원가입</AuthButton>
+        <AuthButton onClick={handleLocalRegister} >회원가입</AuthButton>
         <RightAlignedLink to="/auth/login">로그인</RightAlignedLink>
       </AuthContent>
     );
@@ -142,9 +179,11 @@ export default connect(
   (state) => ({
     form: state.auth.getIn(['register', 'form']),
     error: state.auth.getIn(['register', 'error']),
-    exists: state.auth.getIn(['register', 'exists'])
+    exists: state.auth.getIn(['register', 'exists']),
+    result : state.auth.get('result')
   }),
   (dispatch) => ({
-    AuthActions: bindActionCreators(authActions, dispatch)
+    AuthActions: bindActionCreators(authActions, dispatch),
+    UserActions : bindActionCreators(userActions, dispatch)
   })
 )(Register);
