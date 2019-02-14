e1a3325bec3b7c8c023ff2d731265ee9bfb916b9
belselios@gmail.com <belselios@gmail.com>
2019-02-14 23:00:58 +0900

    유저메뉴 바로 닫히는 문제 수정

--- heurm-client/src/containers/Base/HeaderContainer.js
+++ heurm-client/src/containers/Base/HeaderContainer.js
@@ -1,55 +1,61 @@
-import React, { Component } from 'react';
-import Header, {LoginButton, UserThumbnail} from "components/Base/Header";
+import React, { Component } from "react";
+import Header, { LoginButton, UserThumbnail } from "components/Base/Header";
 import { connect } from "react-redux";
-import * as userActions from 'redux/modules/user';
-import * as baseActions from 'redux/modules/base';
-import { bindActionCreators } from 'redux';
-import storage from 'lib/storage';
+import * as userActions from "redux/modules/user";
+import * as baseActions from "redux/modules/base";
+import { bindActionCreators } from "redux";
+import storage from "lib/storage";
 import UserMenuContainer from "./UserMenuContainer";
 
 class HeaderContainer extends Component {
-  
   handleThumbnailClick = () => {
     const { BaseActions } = this.props;
     BaseActions.setUserMenuVisibility(true);
-  }
-  
+  };
+
   handleLogout = async () => {
     const { UserActions } = this.props;
     try {
       await UserActions.logout();
-    } catch(e) {
+    } catch (e) {
       console.log(e);
     }
 
-    storage.remove('loggedInfo');
-    window.location.href = '/';
-  }
-  
+    storage.remove("loggedInfo");
+    window.location.href = "/";
+  };
+
   render() {
     const { visible, user } = this.props;
     const { handleThumbnailClick } = this;
-    if(!visible) return null;
- 
+    if (!visible) return null;
+
     return (
       <Header>
-        {user.get('logged')
-          ? (<UserThumbnail thumbnail={user.getIn(['loggedInfo', 'thumbnail'])} onClick={handleThumbnailClick} />)
-          : <LoginButton />
-        }
-        <UserMenuContainer eventTypes="click" />
+        {user.get("logged") ? (
+          <UserThumbnail
+            thumbnail={user.getIn(["loggedInfo", "thumbnail"])}
+            onClick={handleThumbnailClick}
+          />
+        ) : (
+          <LoginButton />
+        )}
+        <UserMenuContainer
+          eventTypes="click"
+          outsideClickIgnoreClass="sc-kgoBCf"
+        />
       </Header>
     );
   }
 }
 
 export default connect(
-  (state) => ({
-    visible : state.base.getIn(['header','visible']),
-    user : state.user
+  state => ({
+    visible: state.base.getIn(["header", "visible"]),
+    user: state.user
   }),
-  (dispatch) => ({
-    UserActions : bindActionCreators(userActions, dispatch),
-    BaseActions : bindActionCreators(baseActions, dispatch)
+  dispatch => ({
+    UserActions: bindActionCreators(userActions, dispatch),
+    BaseActions: bindActionCreators(baseActions, dispatch)
   })
 )(HeaderContainer);
