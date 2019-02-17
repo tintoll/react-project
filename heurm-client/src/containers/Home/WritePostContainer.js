743b3003762f5d3a7d1ef07ffe73c073b75a33db
tintoll <belselios@gmail.com>
2019-02-18 08:18:56 +0900

    9-5

--- heurm-client/src/containers/Home/WritePostContainer.js
+++ heurm-client/src/containers/Home/WritePostContainer.js
@@ -1,10 +1,38 @@
 import React, { Component } from "react";
 import WritePost from "components/Home/WritePost";
 
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as homeActions from 'redux/modules/home';
+
 class WritePostContainer extends Component {
+
+  handleChange = (e) => {
+    const { HomeActions } = this.props;
+    HomeActions.changeWritePostInput(e.target.value); //인풋 값 설정 
+  }
+
+  handlePost = () => {
+    // 게이지가 다 차면 실행되는 메소드 
+    const { HomeActions} = this.props;
+    HomeActions.changeWritePostInput(''); //인풋 최기화
+    console.log('posted!'); // 나중에 API 호출 
+  }
+
   render() {
-    return <WritePost />;
+    const { handleChange, handlePost} = this;
+    const {value} = this.props;
+    return (
+      <WritePost value={value} onChange={handleChange} onPost={handlePost} />
+    );
   }
 }
 
-export default WritePostContainer;
+export default connect(
+  (state) => ({
+    value : state.home.getIn(['writePost','value'])
+  }),
+  (dispatch) => ({
+    HomeActions : bindActionCreators(homeActions, dispatch)
+  })
+)(WritePostContainer);
--- heurm-client/src/redux/modules/index.js
+++ heurm-client/src/redux/modules/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import base from "./base";
 import auth from "./auth";
 import user from "./user";
+import home from "./home";
 import { penderReducer } from "redux-pender";
 
 export default combineReducers({
   base,
   auth,
   user,
+  home,
   pender : penderReducer
 });
