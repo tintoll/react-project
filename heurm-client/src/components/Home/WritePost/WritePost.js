a98373dc768c943795fc6b96b4522dc975d37a60
tintoll <belselios@gmail.com>
2019-02-18 08:01:39 +0900

    9-4

--- heurm-client/src/components/Home/WritePost/WritePost.js
+++ heurm-client/src/components/Home/WritePost/WritePost.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import oc from "open-color";
 import { shadow } from "lib/styleUtils";
 import Textarea from "react-textarea-autosize";
+import Progress from "./Progress";
 
 const Wrapper = styled.div`
   width: 768px;
@@ -10,6 +11,7 @@ const Wrapper = styled.div`
   padding: 1rem;
   background: ${oc.gray[7]};
   ${shadow(1)}
+  position : relative;
 `;
 
 // ::placeholder 부분은 아무것도 입력 되지 않은 상태 일 때 폰트색을 정해줍니다.
@@ -29,6 +31,7 @@ const StyledTextarea = styled(Textarea)`
 const WritePost = ({ onChange, value }) => (
   <Wrapper>
     <StyledTextarea minRows={3} maxRows={10} placeholder={`의식의 흐름대로 당신의 생각을 적어보세요.\n5초이상 아무것도 입력하지 않으면 자동으로 포스팅됩니다.`}/>
+    <Progress />
   </Wrapper>
 );
 
