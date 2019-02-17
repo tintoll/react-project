e1d66ae5c736e6dc611a5dff8f51542454f61600
tintoll <belselios@gmail.com>
2019-02-18 08:27:40 +0900

    9-6

--- heurm-client/src/components/Home/WritePost/Progress.js
+++ heurm-client/src/components/Home/WritePost/Progress.js
@@ -20,14 +20,36 @@ class Progress extends Component {
     percentage : 0
   }
 
-  componentDidMount() {
-    this.setState({
-      percentage : 100
-    });
-    setTimeout(() => {
-      console.log('done');
-    }, 1000);
+  timeoutId = null;
 
+  handlePost = () => {
+    const { onPost }= this.props;
+    onPost(); //props로 받은 onPost를 호출합니다.
+  }
+
+  componentWillReceiveProps(nextProps) {
+    // props가 변할때 마다 
+
+    clearTimeout(this.timeoutId); //기존의 타임아웃을 중지시킨다.
+    this.setState({
+      percentage : 0
+    });
+
+    if(nextProps.value === '') {
+      // 내용이 비어있으면 작업을 중단 
+      return;
+    }
+
+    // 상태를 100으로 변경한다. 하나의 이벤트 루프에서 setState가 두번 호출되면
+    // setState를 한번에 하게 되므로 setTimeout으로 감싸줍니다.
+    setTimeout(() => {
+      this.setState({
+        percentage : 100
+      })
+    }, 0);
+
+    // 나중에 취소할수 있도록 this.timeoutId에 setTiemout의 결과를 담는다. 
+    this.timeoutId = setTimeout(this.handlePost, 1000);
   }
 
   render() {
--- heurm-client/src/components/Home/WritePost/WritePost.js
+++ heurm-client/src/components/Home/WritePost/WritePost.js
@@ -28,14 +28,14 @@ const StyledTextarea = styled(Textarea)`
   }
 `;
 
-const WritePost = ({ onChange, value }) => (
+const WritePost = ({ onChange, onPost, value }) => (
   <Wrapper>
     <StyledTextarea 
       onChange={onChange}
       minRows={3} 
       maxRows={10} 
       placeholder={`의식의 흐름대로 당신의 생각을 적어보세요.\n5초이상 아무것도 입력하지 않으면 자동으로 포스팅됩니다.`}/>
-    <Progress />
+    <Progress onPost={onPost} value={value}/>
   </Wrapper>
 );
 
