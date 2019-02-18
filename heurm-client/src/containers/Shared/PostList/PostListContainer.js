5bf76f7c5799fa349361911e5d1e90c0d97d39a9
tintoll <belselios@gmail.com>
2019-02-19 08:19:45 +0900

    10-3

--- heurm-client/src/containers/Shared/PostList/PostListContainer.js
+++ heurm-client/src/containers/Shared/PostList/PostListContainer.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react';
 import PostList from 'components/Shared/PostList';
 
+import {connect} from 'react-redux';
+import { bindActionCreators } from "redux";
+import * as postsActions from 'redux/modules/posts';
+
 class PostListContainer extends Component {
+
+  load = async () => {
+    // 가장 최근 작성된 포스트를 20개 불러옵니다. 
+    const { PostsActions } = this.props;
+    PostsActions.loadPost();
+  }
+
+  componentDidMount () {
+    // 컴포넌트가 마운트 됏을때 호출
+    this.load();
+  }
+
   render() {
     return (
       <PostList />
@@ -10,4 +26,12 @@ class PostListContainer extends Component {
 }
 
 
-export default PostListContainer;
+export default connect(
+  (state) => ({
+    next : state.posts.get('next'),
+    data : state.posts.get('data')
+  }),
+  (dispatch) => ({
+    PostsActions : bindActionCreators(postsActions, dispatch)
+  })
+)(PostListContainer);
