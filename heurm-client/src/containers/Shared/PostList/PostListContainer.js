53e50ed9fdd48316a56586844ea86c6c8dd05a19
tintoll <belselios@gmail.com>
2019-02-20 08:16:26 +0900

    10-7

--- heurm-client/src/containers/Shared/PostList/PostListContainer.js
+++ heurm-client/src/containers/Shared/PostList/PostListContainer.js
@@ -6,16 +6,65 @@ import { bindActionCreators } from "redux";
 import * as postsActions from 'redux/modules/posts';
 
 class PostListContainer extends Component {
+  prev = null;
 
   load = async () => {
     // 가장 최근 작성된 포스트를 20개 불러옵니다. 
     const { PostsActions } = this.props;
-    PostsActions.loadPost();
+
+    try {
+      await PostsActions.loadPost();
+      const {next} = this.props;
+      if(next) {
+        // 다음 불러올 포스트들이 있다면 미리 로딩을 해둔다.
+        await PostsActions.prefetchPost(next);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+    
+  }
+
+  // 다음 목록 불러오기 
+  loadNext = async () => {
+    const { PostsActions, next } = this.props;
+    PostsActions.showPrefetchedPost(); // 미리 불러왔던걸 보여준다
+
+    if(next === this.prev || !next) return; // 이전에 해던 요청과 동일하면 요청하지 않는다.
+    this.prev = next;
+
+    // 다음 데이터 요청
+    try {
+      await PostsActions.prefetchPost(next);
+    } catch (e) {
+      console.log(e);
+    }
+
+    this.handleScroll(); // 한번 더 호출함으로써 인터넷 느린 상황에 밀리는 현상방지 
+  }
+
+  handleScroll = () => {
+    const { nextData } = this.props;
+    if (nextData.size === 0) return; // 미리 불러온 데이터 없으면 작업 중지
+
+    const { innerHeight } = window;
+    const { scrollHeight } = document.body;
+    // IE 에서는 body.scrollTop 대신에 document.documentElement.scrollTop 사용해야함
+    const scrollTop = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+    if(scrollHeight - innerHeight - scrollTop < 100) {
+      this.loadNext();
+    }
   }
 
   componentDidMount () {
     // 컴포넌트가 마운트 됏을때 호출
     this.load();
+    window.addEventListener('scroll',this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    // 컴포넌트가 언마운트 될 때에는 스크롤 이벤트리스너를 제거합니다
+    window.removeEventListener('scroll', this.handleScroll);
   }
 
   render() {
@@ -30,7 +79,8 @@ class PostListContainer extends Component {
 export default connect(
   (state) => ({
     next : state.posts.get('next'),
-    data : state.posts.get('data')
+    data : state.posts.get('data'),
+    nextData : state.posts.get('nextData')
   }),
   (dispatch) => ({
     PostsActions : bindActionCreators(postsActions, dispatch)
--- heurm-client/src/redux/modules/posts.js
+++ heurm-client/src/redux/modules/posts.js
@@ -5,12 +5,18 @@ import * as PostAPI from 'lib/api/posts';
 import { pender } from "redux-pender";
 
 const LOAD_POST = 'posts/LOAD_POST'; // 포스트 리스트 초기 로딩
+const PREFETCH_POST = 'post/PREFETCH_POST'; // 포스트 미리 로딩 
+const SHOW_PREFETCHED_POST = 'post/SHOW_PREFETCHED_POST'; // 미리 로딩된 포스트 화면에 보여주기 
 
 export const loadPost = createAction(LOAD_POST, PostAPI.list);
+export const prefetchPost = createAction(PREFETCH_POST, PostAPI.next); //url
+export const showPrefetchedPost = createAction(SHOW_PREFETCHED_POST);
+
 
 const initialState = Map({
   next : '',
-  data : List()
+  data : List(),
+  nextData : List()
 });
 
 export default handleActions({
@@ -21,5 +27,19 @@ export default handleActions({
       return state.set('next',next)
                   .set('data', fromJS(data));
     }
-  })
+  }),
+  ...pender({
+    type: PREFETCH_POST,
+    onSuccess : (state, action) => {
+      // nextData에 결과를 담아둡니다. 
+      const {next, data} = action.payload.data;
+      return state.set('next', next)
+                  .set('nextData', fromJS(data));
+    }
+  }),
+  [SHOW_PREFETCHED_POST] : (state, action) => {
+    const nextData = state.get('nextData');
+    return state.update('data', data => data.concat(nextData))
+                .set('nextData',List());
+  }
 },initialState);
