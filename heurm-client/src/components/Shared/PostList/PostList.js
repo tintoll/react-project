f0caf10d19ecdf55b7e748f40c5934c3d5e38ca4
belselios@gmail.com <belselios@gmail.com>
2019-02-21 22:16:21 +0900

    오타 수정

--- heurm-client/src/components/Shared/PostList/PostList.js
+++ heurm-client/src/components/Shared/PostList/PostList.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 import Masonry from "react-masonry-component";
-import Post from './Post';
+import Post from "./Post";
 
-
-// 이 컴포넌트에서는 position: relative 속성을 주는것이 중요합니다. 
+// 이 컴포넌트에서는 position: relative 속성을 주는것이 중요합니다.
 // 그 이유는 Post 컴포넌트의 사이즈가 이 컴포넌트의 너비와 비례하여 만들어지기 때문입니다.
 const Wrapper = styled.div`
-  position : relative;
-  margin-top : 1rem;
+  position: relative;
+  margin-top: 1rem;
 `;
 
-const PostList = ({posts}) => {
-  const postList = posts.map((post) => {
-    return <Post key={post.get('_id')} post={post} />
+const PostList = ({ posts }) => {
+  const postList = posts.map(post => {
+    return <Post key={post.get("_id")} post={post} />;
   });
   return (
     <Wrapper>
-      <Masonry otpions={{gutter:16}}>
-        {postList}
-      </Masonry>
+      <Masonry options={{ gutter: 16 }}>{postList}</Masonry>
     </Wrapper>
-)};
+  );
+};
 
 export default PostList;
