ace16baa52dbf0e15d0b2a44473e9de3e6c7c9b4
belselios@gmail.com <belselios@gmail.com>
2019-02-23 22:04:20 +0900

    12-5

--- heurm-client/src/components/Shared/PostList/Post.js
+++ heurm-client/src/components/Shared/PostList/Post.js
@@ -1,102 +1,107 @@
-import React from 'react';
-import styled from 'styled-components';
-import oc from 'open-color';
+import React from "react";
+import styled from "styled-components";
+import oc from "open-color";
 
 // Timeago 라이브러리
-import TimeAgo from 'react-timeago';
+import TimeAgo from "react-timeago";
 import koreanStrings from "react-timeago/lib/language-strings/ko";
 import buildFormatter from "react-timeago/lib/formatters/buildFormatter";
 
 import { media, shadow } from "lib/styleUtils";
+import PostFooter from "./PostFooter";
 
-const formatter = buildFormatter(koreanStrings); //  한글 형식으로 보여주기 
+const formatter = buildFormatter(koreanStrings); //  한글 형식으로 보여주기
 
 const Wrapper = styled.div`
-  width : calc((100% - 32px) / 3);
+  width: calc((100% - 32px) / 3);
   ${media.desktop`
     width : calc((100% - 16px) / 2);
   `}
   ${media.tablet`
     width : 100%;
   `}
-  height : 400px;
-  margin-bottom : 1rem;
-  background : white;
+  
+  margin-bottom: 1rem;
+  background: white;
   ${shadow(1)}
-
 `;
 
 // 포스트 상단 썸네일, 생각번호, 시간을 래핑
 // 자식들을 한줄로 나열 시켜줍니다.
 const PostHead = styled.div`
-  padding : 1rem;
-  display : flex;
-  flex-direction : row;
-  align-items : center;
-  border-bottom : 1px solid ${oc.gray[2]};
+  padding: 1rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  border-bottom: 1px solid ${oc.gray[2]};
 `;
 
 // images props 로 전달받은 썸네일을 32*32 사이즈로 보여줍니다.
 const UserThumbnail = styled.div`
-  background : ${oc.gray[2]};
-  background-image : url(${props => props.image});
-  background-size : cover;
-  background-position : center;
-  background-repeat : no-repeat;
-  width : 32px;
-  height : 32px;
-  border-radius : 50%;
+  background: ${oc.gray[2]};
+  background-image: url(${props => props.image});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
 `;
 
 // 유저네임을 띄워줍니다.
 const Username = styled.div`
-  font-weight : 500;
-  margin-left : 0.3rem;
-  font-size : 0.9rem;
+  font-weight: 500;
+  margin-left: 0.3rem;
+  font-size: 0.9rem;
 `;
 
-// 몇번째 생각이닞 알려줍니다. 
+// 몇번째 생각이닞 알려줍니다.
 const Count = styled.div`
-  color : ${oc.gray[6]};
-  margin-left : 0.3rem;
-  font-size : 0.8rem;
+  color: ${oc.gray[6]};
+  margin-left: 0.3rem;
+  font-size: 0.8rem;
 `;
 
 // 얼마나 전에 작성됐는지 알려줍니다.
 const Time = styled.div`
-  color : ${oc.gray[4]};
-  font-size : 0.8rem;
-  margin-left : auto;
+  color: ${oc.gray[4]};
+  font-size: 0.8rem;
+  margin-left: auto;
 `;
 
-// 포스트 내용을 보여줍니다. 
+// 포스트 내용을 보여줍니다.
 const Content = styled.div`
-  font-size : 1.25rem;
-  color : ${oc.gray[8]};
-  font-weight : 300;
-  padding : 1rem;
-  word-break : break-all;
-  white-space : pre-wrap;
+  font-size: 1.25rem;
+  color: ${oc.gray[8]};
+  font-weight: 300;
+  padding: 1rem;
+  word-break: break-all;
+  white-space: pre-wrap;
 `;
 
-
-
-const Post = ({post}) => {
-  
-  const { count, username, content, 
-    comments, likesCount, createdAt} = post.toJS();
+const Post = ({ post }) => {
+  const {
+    count,
+    username,
+    content,
+    comments,
+    likesCount,
+    createdAt
+  } = post.toJS();
   return (
     <Wrapper>
       <PostHead>
-      <UserThumbnail image={`/api/users/{username}/thumbnail`}/>
+        <UserThumbnail image={`/api/users/{username}/thumbnail`} />
         <Username>{username}</Username>
         <Count>#{count}번째 생각</Count>
-        <Time><TimeAgo date={createdAt} formatter={formatter}/></Time>
+        <Time>
+          <TimeAgo date={createdAt} formatter={formatter} />
+        </Time>
       </PostHead>
-      <Content>
-        {content} 
-      </Content>
+      <Content>{content}</Content>
+      <PostFooter />
     </Wrapper>
-)};
+  );
+};
 
 export default Post;
---
+++ heurm-client/src/components/Shared/PostList/PostFooter.js
@@ -0,0 +1,85 @@
+import React from "react";
+import styled from "styled-components";
+import oc from "open-color";
+
+import { FaHeart } from "react-icons/fa";
+import { FaComments } from "react-icons/fa";
+
+// 테두리, 패딩을 설정하고 내부의 svg와 span 스타일을 설정해줌
+const Wrapper = styled.div`
+  padding: 1rem;
+  border-top: 1px solid ${oc.gray[1]};
+  display: flex;
+  color: ${oc.gray[5]};
+  svg {
+    font-size: 1.75rem;
+    cursor: pointer;
+  }
+  span {
+    margin-left: 0.25rem;
+    font-size: 0.8rem;
+    padding-bottom: 0.25rem;
+  }
+`;
+
+const Likes = styled.div`
+  display: flex;
+  align-items: center;
+  svg {
+    &:hover {
+      color: ${oc.gray[6]};
+    }
+    &:active {
+      color: ${oc.pink[6]};
+    }
+  }
+
+  ${props =>
+    props.active &&
+    `
+    svg {
+      color : ${oc.pink[6]};
+      &:hover {
+        color : ${oc.pink[5]};
+      }
+    }
+  `}
+`;
+
+// 덧글, 우측정렬됨
+const Comments = styled.div`
+  margin-left: auto;
+  display: flex;
+  align-items: center;
+  svg {
+    &:hover {
+      color: ${oc.gray[6]};
+    }
+    &:active {
+      color: ${oc.cyan[6]};
+    }
+  }
+`;
+
+const PostFooter = ({
+  liked,
+  likesCount = 0,
+  comments = [],
+  onToggledLike,
+  onCommentClick
+}) => {
+  return (
+    <Wrapper>
+      <Likes active={liked}>
+        <FaHeart />
+        <span>좋아요 {likesCount}개</span>
+      </Likes>
+      <Comments>
+        <FaComments />
+        <span>덧글 {comments.length}개</span>
+      </Comments>
+    </Wrapper>
+  );
+};
+
+export default PostFooter;
